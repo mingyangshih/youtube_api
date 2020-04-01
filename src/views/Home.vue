e843e56e1da746437393d02adf80e4a61a3f196c
ming <jsps595214@gmail.com>
2020-04-01 13:07:55 +0800

    youtube api start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img :src="item.snippet.thumbnails.default.url" alt="" v-for="(item, idx) in videoary" :key="idx"> -->
+    <!-- <div class="iframe" v-for="(item,idx) in videoary" :key="idx" v-html="item.player.embedHtml"> -->
+    <!-- </div> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+
 
 export default {
+  data() {
+    return {
+      videoary: [],
+    };
+  },
   name: 'Home',
   components: {
-    HelloWorld,
+  },
+  created() {
+    // set fetch 要送出的資料因為GET不能直接帶參數
+    const url = new URL(`${process.env.VUE_APP_VIDEOS_LIST}`);
+    url.search = new URLSearchParams({
+      part: 'snippet,contentDetails,statistics,localizations,player',
+      chart: 'mostpopular',
+      maxResults: 20,
+      h1: 'zh-TW',
+      maxHeight: 72,
+      maxWidth: 100,
+      key: `${process.env.VUE_APP_KEY}`,
+    });
+    // run fetch
+    fetch(url).then((response) => response.json()).then((myJson) => {
+      console.log(myJson);
+      this.videoary = myJson.items;
+    });
   },
 };
 </script>
